75707ddedfc1b655c184c325dae274794373db59
mohsinamumtaz <mohsina.mumtaz@gmail.com>
2026-04-09 15:38:21 +0530

    only one instance

--- instance.tf
+++ instance.tf
@@ -13,16 +13,3 @@ Name = "MY-EC2-Instance"
 }
 
 
-resource "aws_instance" "My-instance" {
-
-ami = "ami-048f4445314bcaa09"
-
-instance_type = "t3.micro"
-
-tags = {
-
-Name = "MY-EC2-Instance"
-
-}
-
-}
